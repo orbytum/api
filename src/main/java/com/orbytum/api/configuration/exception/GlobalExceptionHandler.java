b16749d0f77d2b8c799566dfc3fe99e8407f4c5f
victorbetoni <victorbetoni@protonmail.com>
2026-08-14 11:37:07 -0300

    (feat) implementando service para envio de emails

--- src/main/java/com/orbytum/api/configuration/exception/GlobalExceptionHandler.java
+++ src/main/java/com/orbytum/api/configuration/exception/GlobalExceptionHandler.java
@@ -20,4 +20,14 @@ public class GlobalExceptionHandler {
         );
         return ResponseEntity.status(HttpStatus.CONFLICT).body(erro);
     }
+
+    @ExceptionHandler(com.orbytum.api.models.exceptions.EmailSendingException.class)
+    public ResponseEntity<ErroResponse> handleEmailSendingException(com.orbytum.api.models.exceptions.EmailSendingException ex) {
+        ErroResponse erro = new ErroResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Falha ao enviar e-mail: " + ex.getMessage(),
+                System.currentTimeMillis()
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(erro);
+    }
 }
